081795e863e1332bcddfb1a7fdcb58306e48aced
vreeslike <39638621+vreeslike@users.noreply.github.com>
2018-06-06 16:11:49 -0400

    Add files via upload

---
+++ tests/k_canalyzing_tests.py
@@ -0,0 +1,24 @@
+"""Program to test validity of functions for randomly creating k-canalyzing functions"""
+import matplotlib.pyplot as plt
+import scipy.stats as sps
+import partition
+
+def partition_test(num_vars, num_points):
+    """Method to test uniformity of random partitions"""
+    data = dict()
+    for _ in range(int(partition.fubini(num_vars)) * num_points):
+        variables = range(num_vars)
+        part = partition.random_partition(variables)
+        key = " ".join(["".join([str(j) for j in i]) for i in part])
+        if key in data:
+            data[key] += 1
+        else:
+            data[key] = 1
+    data_list = data.values()
+    plt.bar(range(len(data)), list(data.values()), align='center')
+    plt.xticks(range(len(data)), list(data.keys()))
+    print sps.chisquare(data_list)
+    plt.show()
+
+## Test case ##
+partition_test(5, 100)
