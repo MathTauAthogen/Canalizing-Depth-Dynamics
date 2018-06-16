2c1cb668aeed4632413efb90c1fbd4cb19b8b815
vreeslike <39638621+vreeslike@users.noreply.github.com>
2018-06-15 20:01:30 -0400

    Added test for k_canalyzing as well

--- tests/k_canalyzing_tests.py
+++ tests/k_canalyzing_tests.py
@@ -5,6 +5,7 @@ import scipy.stats as sps
 import sys
 sys.path.insert(0, '../')
 import partition
+import k_canalyzing_faulty as kc
 
 def partition_test(num_vars, num_points):
     """Method to test uniformity of random partitions"""
@@ -19,11 +20,30 @@ def partition_test(num_vars, num_points):
             data[key] = 1
     data_list = data.values()
     plt.bar(range(len(data)), list(data.values()), align='center')
-    plt.xticks(range(len(data)), list(data.keys()))
     difference = len(data) - partition.fubini(num_vars)
     print "Found number of functions - number of desired functions : " + str(difference)
     print sps.chisquare(data_list)
     plt.show()
 
+def k_canalyzing_test(num_vars, depth, num_points):
+    """Method to test distribution of random_k_canalyzing"""
+    data = dict()
+    for _ in range(num_points * (2 ** num_vars)):
+        func = kc.random_k_canalyzing(num_vars, depth)
+        table = func.return_truth_table()
+        key = "".join([str(i) for i in table])
+        if key in data:
+            data[key] += 1
+        else:
+            data[key] = 1
+    data_list = data.values()
+    plt.bar(range(len(data)), list(data.values()), align='center')
+    print "Functions found: " + str(len(data))
+    print sps.chisquare(data_list)
+    for i in data:
+        print i + "  " + str(data[i])
+    plt.show()
+    
 ## Test case ##
-partition_test(5, 100)
+#partition_test(5, 100)
+k_canalyzing_test(2, 2, 1000)
