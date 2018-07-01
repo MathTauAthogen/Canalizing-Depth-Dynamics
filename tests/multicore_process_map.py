15a5199aa99e23a1db7b01b3de741aa0adcb3797
Gleb Pogudin <pogudin.gleb@gmail.com>
2018-06-30 23:51:28 -0400

    style fixes in multicore_process_map.py

--- tests/multicore_process_map.py
+++ tests/multicore_process_map.py
@@ -16,35 +16,31 @@ import random
 
 #start=time.time()
 parser = argparse.ArgumentParser(description='Make a discrete dynamical system at random given the number of variables and canalyzing depth.')
-parser.add_argument('num', type= int)
-parser.add_argument('cores', type=int)
-parser.add_argument('num_vars', type= int)
-parser.add_argument('canalyzing_depth', type=int)
+parser.add_argument('num', type = int)
+parser.add_argument('cores', type = int)
+parser.add_argument('num_vars', type = int)
+parser.add_argument('canalyzing_depth', type = int)
 args = parser.parse_args()
 
 def threadfunc(seed):
     random.seed(seed)
-    funcs=[]
+    funcs = []
     for i in range(args.num_vars):
         funcs.append(kc.random_k_canalyzing(args.num_vars, args.canalyzing_depth).return_truth_table())
-    tuples=fad.find_attractors_and_basins(funcs)
+    tuples = fad.find_attractors_and_basins(funcs)
     return tuples
-    #with open("num_vars="+str(args.num_vars)+"_depth="+str(args.canalyzing_depth)+".txt", "a") as file:
-    #    print >>file, str(tuples)
 
-start=time.time()
+start = time.time()
 pool = Pool(args.cores)
-filename="num_vars="+str(args.num_vars)+"_depth="+str(args.canalyzing_depth)+".json"
-loaded=[]
+filename = "num_vars=" + str(args.num_vars) + "_depth=" + str(args.canalyzing_depth) + ".json"
+loaded = []
 if(os.path.exists(filename)):
 	with open(filename, "r") as file:
-		loaded=json.load(file)
-n=len(loaded)+1
-outputs=pool.map(threadfunc, xrange(n,n+args.num))
+		loaded = json.load(file)
+num_already_computed = len(loaded) + 1
+outputs = pool.map(threadfunc, xrange(num_already_computed, num_already_computed + args.num))
 with open(filename, "w") as file:
-    loaded+=outputs
+    loaded += outputs
     json.dump(loaded, file)
-end=time.time()
-print(end-start)
-#end=time.time()
-#print(end-start)
+end = time.time()
+print(end - start)
